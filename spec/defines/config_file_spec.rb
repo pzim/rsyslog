7d19cb1326b2380e4a1825a4a78490acf0da67ba
pzimmer <phil.zimmerman@twcable.com>
2014-02-13 10:27:24 -0700

    make config_file_spec test more generic

--- spec/defines/config_file_spec.rb
+++ spec/defines/config_file_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'rsyslog::config_file' do
 
-    let(:title) { '/etc/rsyslog.d/tomcatForward.conf' }
+    let(:title) { '/etc/rsyslog.d/logForward.conf' }
     let(:params) { 
       {:config_hash => {'audit' => 
         {'InputFileName'      => '/data/logs/someapp/audit.log',
@@ -15,12 +15,12 @@ describe 'rsyslog::config_file' do
       }
     }
 
-    it "should contain /etc/rsyslog.d/tomcatForward.conf" do
-      should contain_file('/etc/rsyslog.d/tomcatForward.conf').with_content(/$*InputFileName \/data\/logs\/someapp\/audit.log/)
-      should contain_file('/etc/rsyslog.d/tomcatForward.conf').with_content(/$*InputFileTag someapp-audit/)
-      should contain_file('/etc/rsyslog.d/tomcatForward.conf').with_content(/$*InputFileStateFile someapp-audit-state/)
-      should contain_file('/etc/rsyslog.d/tomcatForward.conf').with_content(/$*InputFileSeverity info/)
-      should contain_file('/etc/rsyslog.d/tomcatForward.conf').with_content(/$*InputFileFacility local6/)
+    it "should contain /etc/rsyslog.d/logForward.conf" do
+      should contain_file('/etc/rsyslog.d/logForward.conf').with_content(/$*InputFileName \/data\/logs\/someapp\/audit.log/)
+      should contain_file('/etc/rsyslog.d/logForward.conf').with_content(/$*InputFileTag someapp-audit/)
+      should contain_file('/etc/rsyslog.d/logForward.conf').with_content(/$*InputFileStateFile someapp-audit-state/)
+      should contain_file('/etc/rsyslog.d/logForward.conf').with_content(/$*InputFileSeverity info/)
+      should contain_file('/etc/rsyslog.d/logForward.conf').with_content(/$*InputFileFacility local6/)
     end
 end
 
